c2892de1f69d72dcf11e86b5e6f0b25e7c1fcb72
Sean Blessing <snblessing@gmail.com>
2019-09-12 10:04:05 -0400

    in class demo of homework

--- stuffy-dispenser/js/ui/stuffy.component.js
+++ stuffy-dispenser/js/ui/stuffy.component.js
@@ -1,9 +1,44 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var readlineSync = require('readline-sync');
 var stuffy_class_1 = require("../model/stuffy.class");
 console.log('Welcome to the stuffy dispenser app!');
-// create instance of stuffy
+// create instances of stuffy
 var s1 = new stuffy_class_1.Stuffy(1, "giraffe", "blue", "s", 3);
-// display instance of stuffy
-console.log(s1.about());
+var s2 = new stuffy_class_1.Stuffy(5, "lion", "brown", "l", 4);
+var s3 = new stuffy_class_1.Stuffy(9, "bear", "yello", "l", 2);
+var s4 = new stuffy_class_1.Stuffy(13, "platypus", "yellow", "s", 2);
+var s5 = new stuffy_class_1.Stuffy(19, "octopus", "hot pink", "xl", 22);
+var s6 = new stuffy_class_1.Stuffy(21, "shark", "grey", "l", 0);
+// create an array of stuffies
+var stuffies = [s1, s2, s3, s4, s5, s6];
+console.log('list of all stuffies:');
+console.log('id\ttype\t\tcolor\tsize\tlimbs');
+console.log('==\t========\t\t=====\t====\t=====');
+for (var i = 0; i < stuffies.length; i++) {
+    //console.log(stuffies[i].about());
+    var stuffy = stuffies[i];
+    console.log(stuffy.id + '\t' + stuffy.type + '\t\t' + stuffy.color +
+        '\t' + stuffy.size + '\t' + stuffy.limbs);
+}
+// console.log(stuffies);
+// prompt user for id to get
+var id = readlineSync.questionInt('Which id would you like to get?');
+// display instance of stuffy - one way
+stuffies.forEach(function (s) {
+    if (s.id == id) {
+        console.log(s.about());
+    }
+});
+// call getStuffy function
+getStuffy(id);
+function getStuffy(i) {
+    // display instance of stuffy - 2nd way
+    for (var _i = 0, stuffies_1 = stuffies; _i < stuffies_1.length; _i++) {
+        var s = stuffies_1[_i];
+        if (s.id == i) {
+            console.log(s.about());
+        }
+    }
+}
 console.log('bye!');
--- stuffy-dispenser/ts/ui/stuffy.component.ts
+++ stuffy-dispenser/ts/ui/stuffy.component.ts
@@ -1,8 +1,48 @@
+var readlineSync = require('readline-sync');
 import { Stuffy } from "../model/stuffy.class";
 console.log('Welcome to the stuffy dispenser app!');
-// create instance of stuffy
+// create instances of stuffy
 let s1: Stuffy = new Stuffy(1,"giraffe","blue","s",3);
-// display instance of stuffy
-console.log(s1.about());
+let s2: Stuffy = new Stuffy(5,"lion","brown","l",4);
+let s3: Stuffy = new Stuffy(9,"bear","yello","l",2);
+let s4: Stuffy = new Stuffy(13,"platypus","yellow","s",2);
+let s5: Stuffy = new Stuffy(19,"octopus","hot pink","xl",22);
+let s6: Stuffy = new Stuffy(21,"shark","grey","l",0);
+
+// create an array of stuffies
+let stuffies: Stuffy[] = [s1,s2,s3,s4,s5,s6];
+
+console.log('list of all stuffies:');
+console.log('id\ttype\t\tcolor\tsize\tlimbs');
+console.log('==\t========\t\t=====\t====\t=====');
+for (let i: number = 0; i < stuffies.length; i++) {
+    //console.log(stuffies[i].about());
+    let stuffy: Stuffy = stuffies[i];
+    console.log(stuffy.id+'\t'+stuffy.type+'\t\t'+stuffy.color+
+                '\t'+stuffy.size+'\t'+stuffy.limbs);
+}
+// console.log(stuffies);
+
+// prompt user for id to get
+let id: number = readlineSync.questionInt('Which id would you like to get?');
+
+// display instance of stuffy - one way
+stuffies.forEach(s => {
+    if (s.id==id) {
+        console.log(s.about());
+    }
+});
+
+// call getStuffy function
+getStuffy(id);
+
+function getStuffy(i: number): void {
+    // display instance of stuffy - 2nd way
+    for (let s of stuffies) {
+        if (s.id==i) {
+            console.log(s.about());
+        }
+    }
+}
 
 console.log('bye!');
